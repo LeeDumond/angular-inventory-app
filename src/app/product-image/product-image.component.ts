eff45b75abe03ff956740b513d3668be4bcd461f
Lee Dumond <leedumond@gmail.com>
2018-02-08 19:44:12 -0600

    add product-image

---
+++ src/app/product-image/product-image.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, HostBinding } from '@angular/core';
+import { Product } from '../product.model';
+
+@Component({
+  selector: 'app-product-image',
+  templateUrl: './product-image.component.html',
+  styleUrls: ['./product-image.component.css']
+})
+export class ProductImageComponent {
+
+  constructor() { }
+  @Input() product: Product;
+  @HostBinding()('attr.class') cssClass = 'ui small image';
+
+
+}
